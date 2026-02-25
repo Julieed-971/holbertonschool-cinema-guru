ee60c233770d740cc314d0821e0d066bce09dd53
Julie Dedieu <julie.dedieu1@gmail.com>
2026-02-25 14:55:55 +0100

    Authentification - Api integration

--- src/routes/auth/Authentication.jsx
+++ src/routes/auth/Authentication.jsx
@@ -2,6 +2,7 @@ import './auth.css';
 import Login from './Login'
 import Register from './Register'
 import { useState } from 'react';
+import axios from 'axios';
 import Button from './../../components/general/Button'
 
 export default function Authentication(
@@ -14,9 +15,33 @@ export default function Authentication(
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
+    const handleSubmit = async (onSubmit) => {
+        onSubmit.preventDefault();
+        try {
+            if (_switch) {
+                const request = await axios.post('/api/auth/login', { username, password })
+                    if (request.status === 200) {
+                    const accessToken = request.data.accessToken;
+                    localStorage.setItem('accessToken', accessToken)
+                    setUserUsername(username)
+                    setIsLoggedIn(true)
+                }
+            } else {
+                const request = await axios.post('/api/auth/register', {username, password});
+                if (request.status === 200) {
+                    const accessToken = request.data.accessToken;
+                    localStorage.setItem('accessToken', accessToken)
+                    setUserUsername(username)
+                    setIsLoggedIn(true)
+                }
+            }
+        } catch (error) {
+            console.error('Error during login or register', error);
+        }
+    }
     return (
-        <div className="login-register-form-container">
-            <form >
+        <div className="login-register-form-container" >
+            <form onSubmit={handleSubmit}>
                 <div className="login-register-form-buttons">
                     <Button className={`sign-in ${_switch ? 'active' : ''}`} label="Sign In" onClick={() => setSwitch(true)} />
                     <Button className={`sign-up ${!_switch ? 'active' : ''}`} label="Sign Up" onClick={() => setSwitch(false)} />
